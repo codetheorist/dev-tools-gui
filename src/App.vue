49f7209a63dd6f28650acff7c334ef1cb50507ba
Alex Scott <codetheorist@outlook.com>
2021-12-17 14:46:38 +0000

    Install Bootstrap Vue Install Popper.JS Install Portal Vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-navbar>
+      <b-navbar-brand> CodeTheorist Dev Tools </b-navbar-brand>
+    </b-navbar>
+    <b-container fluid>
+      <b-row>
+        <b-col cols="12">
+          <img alt="Vue logo" src="./assets/logo.png" />
+          <HelloWorld msg="Welcome to Your Vue.js App" />
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
