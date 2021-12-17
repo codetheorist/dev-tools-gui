4af29fa13a609ed161fb7d543d8c912eb0c4ae17
Alex Scott <codetheorist@outlook.com>
2021-12-17 14:29:00 +0000

    VS Code Debug Config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,13 @@
 module.exports = {
+  configureWebpack: {
+    devtool: 'source-map'
+  },
   pluginOptions: {
     electronBuilder: {
       builderOptions: {
+        appId: "com.codetheorist.dev-tools.gui",
+        copyright: "Copyright © 2021 CodeTheorist",
+        productName: "CodeTheorist Dev Tools",
         publish: ['github']
       }
     }
